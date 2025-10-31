7ff54bc434b53ff9adf12d838de8b2614bc055df
Taliyah Evans <tevans44@broncos.uncfsu.edu>
2025-10-31 11:10:24 -0400

    Completed all TODOs for LabAssignment11

--- rectangle_info.py
+++ rectangle_info.py
@@ -1,5 +1,5 @@
 # TODO: complete the function definition in the next line
-def rectangle_info():
+def rectangle_info(width, height):
     """
     Calculate area, perimeter, and diagonal of a rectangle.
 
@@ -16,6 +16,8 @@ def rectangle_info():
     diagonal = (width**2 + height**2) ** 0.5
 
     # TODO: return three calculated variables
+    return area, perimeter, diagonal
+
 
 
 if __name__ == "__main__":
@@ -23,4 +25,5 @@ if __name__ == "__main__":
     h = float(input("Enter height: "))
 
     # TODO: call function rectangle_info() and pass two arguments: w and h.
-    print(f"Area: {rect_a}, Perimeter: {rect_p}, Diagonal: {rect_d}")
+    rect_area, rect_perimeter, rect_diagonal = rectangle_info(w, h)
+    print(f"Area: {rect_area}, Perimeter: {rect_perimeter}, Diagonal: {rect_diagonal}")
